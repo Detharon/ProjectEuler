46fe8a3b7833c0e4dbd24af71d2480a84615be84
Dth <detharon@gmail.com>
2017-07-29 20:43:10 +0200

    Refactor problem 11, add missing build files

--- src/main/scala/Euler011.scala
+++ src/main/scala/Euler011.scala
@@ -1,36 +1,30 @@
-import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 object Euler011 extends EulerApp {
 
     def prepareData(): Array[Array[Int]] = {
-        val data: ArrayBuffer[Array[Int]] = ArrayBuffer()
-        for (line <- Source.fromURL(getClass.getResource("Euler011.data")).getLines()) {
-            data += line.split(" ").map(_.toInt)
-        }
-
-        data.toArray
+        (for (line <- Source.fromURL(getClass.getResource("Euler011.data")).getLines())
+            yield line.split(" ").map(_.toInt)).toArray
     }
-
 
     def findLagestProduct(matrix: Array[Array[Int]]): Long = {
         var max: Long = 0
 
-        for (i <- 0 until matrix.length; j <- 0 until matrix(i).length) {
-            if (i < matrix.length - 3) {
-                max = Math.max(max, matrix(i)(j) * matrix(i + 1)(j) * matrix(i + 2)(j) * matrix(i + 3)(j))
+        for (x <- 0 until matrix.length; j <- 0 until matrix(x).length) {
+            if (x < matrix.length - 3) {
+                max = Math.max(max, matrix(x)(j) * matrix(x + 1)(j) * matrix(x + 2)(j) * matrix(x + 3)(j))
             }
 
-            if (j < matrix(i).length - 3) {
-                max = Math.max(max, matrix(i)(j) * matrix(i)(j+1) * matrix(i)(j+2) * matrix(i)(j+3))
+            if (j < matrix(x).length - 3) {
+                max = Math.max(max, matrix(x)(j) * matrix(x)(j+1) * matrix(x)(j+2) * matrix(x)(j+3))
             }
 
-            if ((j < matrix(i).length - 3) && (i > 3)) {
-                max = Math.max(max, matrix(i)(j) * matrix(i-1)(j+1) * matrix(i-2)(j+2) * matrix(i-3)(j+3))
+            if ((j < matrix(x).length - 3) && (x > 3)) {
+                max = Math.max(max, matrix(x)(j) * matrix(x-1)(j+1) * matrix(x-2)(j+2) * matrix(x-3)(j+3))
             }
 
-            if ((j < matrix(i).length - 3) && (i < matrix.length - 3)) {
-                max = Math.max(max, matrix(i)(j) * matrix(i+1)(j+1) * matrix(i+2)(j+2) * matrix(i+3)(j+3))
+            if ((j < matrix(x).length - 3) && (x < matrix.length - 3)) {
+                max = Math.max(max, matrix(x)(j) * matrix(x+1)(j+1) * matrix(x+2)(j+2) * matrix(x+3)(j+3))
             }
         }
 
