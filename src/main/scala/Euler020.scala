a2c47f630facf01551e316df9d8df6d2720fbb75
Dth <detharon@gmail.com>
2018-08-05 08:56:36 +0200

    Add 020 & factorial() method

---
+++ src/main/scala/Euler020.scala
@@ -0,0 +1,12 @@
+import EulerHelper._
+
+object Euler020 extends EulerApp {
+  override def execute(): Any = {
+    readDigits(BigInt(100).factorial()) sum
+  }
+
+  def readDigits(n: BigInt): Stream[Short] = {
+    if (n == 0) Stream.Empty
+    else (n % 10).toShort #:: readDigits(n / 10)
+  }
+}
--- src/main/scala/EulerHelper.scala
+++ src/main/scala/EulerHelper.scala
@@ -1,8 +1,20 @@
+import scala.annotation.tailrec
+import scala.math.BigInt
+
 object EulerHelper {
 
   implicit class BigIntExp(n: BigInt) {
     def **(power: Int): BigInt = {
       n.pow(power)
+    }
+
+    def factorial(): BigInt = {
+      @tailrec
+      def factorial(n: BigInt, acc: BigInt): BigInt = {
+        if (n == 0) acc
+        else factorial(n - 1, acc * n)
+      }
+      factorial(n, 1)
     }
   }
 
