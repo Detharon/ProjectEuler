49afedf91518aebd18d4ff89a36d71f8708b53fa
Dth <detharon@gmail.com>
2018-08-05 08:08:12 +0200

    Improve 001 solution

--- src/main/scala/Euler001.scala
+++ src/main/scala/Euler001.scala
@@ -1,5 +1,9 @@
 object Euler001 extends EulerApp {
     override def execute(): Int = {
-        1 until 1000 filter {i => ((i % 3 == 0) || (i % 5 == 0))} sum
+        val threes = 3 until 1000 by 3 sum
+        val fives = 5 until 1000 by 5 sum
+        val fifteens = 15 until 1000 by 15 sum
+
+        threes + fives - fifteens
     }
 }
