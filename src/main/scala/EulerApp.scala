195be4af4e4f90778b7ce513caada93750b1b0fb
Detharon <detharon@gmail.com>
2020-10-17 11:43:09 +0200

    Add explanation to Problem 1, refactor EulerApp

--- src/main/scala/EulerApp.scala
+++ src/main/scala/EulerApp.scala
@@ -1,17 +1,20 @@
 abstract class EulerApp {
   def execute(): Any
+  def alternative(): Any = {}
 
   def main(args: Array[String]): Unit = {
     warmUp()
-
-    val start = System.currentTimeMillis()
-    println(execute())
-    val time = System.currentTimeMillis() - start
-
-    println(s"Elapsed time: $time ms")
+    measure(execute)
   }
 
   def warmUp(): Unit = {
     (0 to 25).foreach(_ ^ 5)
+  }
+
+  def measure(f: () => Any): Unit = {
+    val start = System.currentTimeMillis()
+    println(s"Result: ${f()}")
+    val time = System.currentTimeMillis() - start
+    println(s"Elapsed time: $time ms")
   }
 }
