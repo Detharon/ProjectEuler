5ed97c6f7cef8f0b81b8a9e2a55fe307bbbc431f
Piotr Strąk <piotr@maropost.com>
2024-11-14 15:02:28 +0100

    Remove deprecated code from Euler030

--- src/main/scala/Euler030.scala
+++ src/main/scala/Euler030.scala
@@ -3,17 +3,17 @@ import EulerHelper._
 object Euler030 extends EulerApp {
 
   override def execute(): Int = {
-    Stream.from(2)
+    LazyList.from(2)
       .takeWhile(_ < 354285)
       .filter(n => digitsToPower(n, 5) == n)
       .sum
   }
 
-  def digitsToPower(n: BigInt, power: Int): BigInt = {
+  private def digitsToPower(n: BigInt, power: Int): BigInt = {
     getDigits(n).map(BigInt(_) ** power).sum
   }
 
-  def getDigits(n: BigInt): List[Int] = {
+  private def getDigits(n: BigInt): List[Int] = {
     n.toString().map(_.asDigit).toList
   }
 }
