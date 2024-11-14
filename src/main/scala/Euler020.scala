ee4c5f529c289ab244121ea1b3b1fdcfbeb766a5
Piotr Strąk <piotr@maropost.com>
2024-11-14 14:52:20 +0100

    Remove deprecated code from Euler020

--- src/main/scala/Euler020.scala
+++ src/main/scala/Euler020.scala
@@ -1,14 +1,12 @@
 import EulerHelper._
 
-import scala.language.postfixOps
-
 object Euler020 extends EulerApp {
   override def execute(): Any = {
-    readDigits(BigInt(100).factorial()) sum
+    readDigits(BigInt(100).factorial()).sum
   }
 
-  def readDigits(n: BigInt): Stream[Short] = {
-    if (n == 0) Stream.Empty
+  private def readDigits(n: BigInt): LazyList[Short] = {
+    if (n == 0) LazyList()
     else (n % 10).toShort #:: readDigits(n / 10)
   }
 }
