68e38df7b978e60cc18f0294d3e32fa42ab83c02
Piotr Strąk <piotr@maropost.com>
2024-11-14 14:49:34 +0100

    Remove deprecated code from EulerApp

--- src/main/scala/EulerApp.scala
+++ src/main/scala/EulerApp.scala
@@ -1,17 +1,16 @@
 abstract class EulerApp {
   def execute(): Any
-  def alternative(): Any = {}
 
   def main(args: Array[String]): Unit = {
     warmUp()
-    measure(execute _)
+    measure(execute)
   }
 
-  def warmUp(): Unit = {
+  private def warmUp(): Unit = {
     (0 to 25).foreach(_ ^ 5)
   }
 
-  def measure(f: () => Any): Unit = {
+  private def measure(f: () => Any): Unit = {
     val start = System.currentTimeMillis()
     println(s"Result: ${f()}")
     val time = System.currentTimeMillis() - start
