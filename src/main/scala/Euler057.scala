24acb383cfc5150c329ca29a7eae2b20427e8ab7
Piotr Strąk <piotr@maropost.com>
2024-11-20 15:21:30 +0100

    Add solution for problem 57

---
+++ src/main/scala/Euler057.scala
@@ -0,0 +1,14 @@
+import scala.annotation.tailrec
+
+object Euler057 extends EulerApp {
+
+  def execute(): Int = (1 to 1000).map(expansion(_, 1, 2)).count { case (numerator, denominator) =>
+    numerator.toString.length > denominator.toString.length
+  }
+
+  @tailrec
+  private def expansion(depth: Int, numerator: BigInt, denominator: BigInt): (BigInt, BigInt) = {
+    if (depth == 1) (numerator + denominator, denominator)
+    else expansion(depth - 1, denominator, numerator + 2 * denominator)
+  }
+}
