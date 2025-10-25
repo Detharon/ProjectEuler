4bbc3c46cc8ba02d067c1f96da6d1fe3db00b241
Piotr Strąk <piotr@maropost.com>
2025-10-25 19:52:26 +0200

    Replace LazyList with an Iterator * Due to lack of memoization it's almost three times faster

--- src/main/scala/Euler007.scala
+++ src/main/scala/Euler007.scala
@@ -1,3 +1,5 @@
+import EulerHelper.isPrime
+
 /** Problem 7: By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
  *
  * What is the 10001st prime number?
@@ -5,21 +7,16 @@
  * --------------------------------------------------
  *
  * Explanation: We can simply brute-force this problem by using the most naive prime check, 
- * since the 10001st prime will be a low number.
- * This solution is pretty fast, even though the algorithm is not very efficient.
+ * since the 10001st prime will be a low number. This solution is pretty fast, even though the algorithm is not very efficient.
+ *
  * As you can see, we don't even filter out even numbers higher than 2 (or all the multiplies of already found primes),
  * but we'll need these methods later on. For now, a basic algorithm does pretty well.
  *
- * LazyList here is a nice way to keep the code functional without storing 10000 elements in memory.
  */
 object Euler007 extends EulerApp {
 
-  override def execute(): Any = LazyList.from(2)
+  override def execute(): Any = Iterator.from(2)
     .filter(isPrime)
     .drop(10000)
-    .head
-
-  private def isPrime(n: Long): Boolean = n match
-    case n if n < 2 => false
-    case _ => (2 to Math.sqrt(n.toDouble).toInt).forall(n % _ != 0)
+    .next()
 }
--- src/main/scala/EulerHelper.scala
+++ src/main/scala/EulerHelper.scala
@@ -3,6 +3,20 @@ import scala.math.BigInt
 
 object EulerHelper {
 
+  extension (n: Int) {
+    def isPrime: Boolean = n match {
+      case n if n < 2 => false
+      case _ => (2 to Math.sqrt(n.toDouble).toInt).forall(n % _ != 0)
+    }
+  }
+
+  extension (n: Long) {
+    def naiveIsPrime: Boolean = n match {
+      case n if n < 2 => false
+      case _ => (2L to Math.sqrt(n.toDouble).toLong).forall(n % _ != 0)
+    }
+  }
+
   extension (n: BigInt) {
     def **(power: Int): BigInt = n.pow(power)
 
