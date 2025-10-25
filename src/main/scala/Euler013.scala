856671c4759c06a2981529d6690f9b1e4c713697
Piotr Strąk <piotr@maropost.com>
2025-10-25 19:08:56 +0200

    Fix formatting of problem 13 and standardize the dataset

--- src/main/scala/Euler013.scala
+++ src/main/scala/Euler013.scala
@@ -1,12 +1,10 @@
-import scala.io.Source
-
 object Euler013 extends EulerApp {
   override def execute(): String = {
     val data = prepareData()
 
     var sum: Long = 0;
-    for (column <- (0 until data(0).length) if (sum < 10000000000000L)) {
-      for (row <- (0 until data.length)) {
+    for (column <- data(0).indices if sum < 10000000000000L) {
+      for (row <- data.indices) {
         sum += data(row)(column)
       }
       sum *= 10
@@ -16,7 +14,9 @@ object Euler013 extends EulerApp {
   }
 
   def prepareData(): Array[Array[Int]] = {
-    (for (line <- Source.fromURL(getClass.getResource("Euler013.data")).getLines())
-      yield line.split("").map(_.toInt)).toArray
+    val data = for {
+      line <- loadFileAsLines()
+    } yield line.split("").map(_.toInt)
+    data.toArray
   }
 }
