009b226cf9c0d980b524fe8a70d5f77cde85a84d
Piotr Strąk <piotr@maropost.com>
2025-11-01 10:13:59 +0100

    Improve Euler003

--- src/main/scala/Euler003.scala
+++ src/main/scala/Euler003.scala
@@ -1,3 +1,5 @@
+import EulerHelper.isPrime
+
 import scala.annotation.tailrec
 
 object Euler003 extends EulerApp {
@@ -12,27 +14,12 @@ object Euler003 extends EulerApp {
    * The numbers are low enough that we don't need any optimisations to solve it in a matter of milliseconds.
    *
    */
-  override def execute(): Long = nextPrimeFactor(600851475143L, primes.iterator)
+  override def execute(): Long = primeFactors(600851475143L).max
 
   @tailrec
-  private def nextPrimeFactor(n: Long, primesIterator: Iterator[Int]): Long = {
-    val currentPrime = primesIterator.next()
+  private def primeFactors(n: Long, i: Int = 2, primesFound: List[Int] = List.empty): List[Int] =
+    if (n == 1) primesFound
+    else if (n % i == 0 && isPrime(i)) primeFactors(n / i, i + 1, primesFound :+ i)
+    else primeFactors(n, i + 1, primesFound)
 
-    if (isPrime(n)) n
-    else {
-      if (n % currentPrime == 0) nextPrimeFactor(n / currentPrime, primesIterator)
-      else nextPrimeFactor(n, primesIterator)
-    }
-  }
-
-  private val primes: LazyList[Int] = 2 #:: nextPrime(3)
-
-  private def nextPrime(from: Int): LazyList[Int] = {
-    if (isPrime(from)) from #:: nextPrime(from + 1)
-    else nextPrime(from + 1)
-  }
-
-  private def isPrime(n: Long): Boolean = n match
-    case n if n < 2 => false
-    case _ => (2 to Math.sqrt(n.toDouble).toInt).forall(n % _ != 0)
 }
