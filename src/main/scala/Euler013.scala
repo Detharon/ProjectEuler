7a510337c00f886a71b962eebaf7376dae0aae5a
Piotr Strąk <piotr@maropost.com>
2025-11-12 20:50:12 +0100

    Simplify solution of problem 13

--- src/main/scala/Euler013.scala
+++ src/main/scala/Euler013.scala
@@ -1,22 +1,4 @@
 object Euler013 extends EulerApp {
-  override def execute(): String = {
-    val data = prepareData()
-
-    var sum: Long = 0;
-    for (column <- data(0).indices if sum < 10000000000000L) {
-      for (row <- data.indices) {
-        sum += data(row)(column)
-      }
-      sum *= 10
-    }
-
-    sum.toString.substring(0, 10)
-  }
-
-  def prepareData(): Array[Array[Int]] = {
-    val data = for {
-      line <- loadFileAsLines()
-    } yield line.split("").map(_.toInt)
-    data.toArray
-  }
+  override def execute(): String =
+    loadFileAsLines().map(BigInt.apply).sum.toString().take(10)
 }
