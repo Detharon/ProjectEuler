15daf3de6f0a7747c2e30f49fb06432d49befb27
Piotr Strąk <piotr@maropost.com>
2025-11-23 09:39:44 +0100

    Fix empty solution of problem 67

--- src/main/scala/Euler067.scala
+++ src/main/scala/Euler067.scala
@@ -1,3 +1,15 @@
-object Euler067 {
+object Euler067 extends EulerApp {
+  override def execute(): Any = {
+    val reversedData = prepareData().reverse
+    reversedData.tail
+      .foldLeft(reversedData.head) { case (previousRow, currentRow) =>
+        currentRow.zipWithIndex.map { case (element, index) =>
+          element + Math.max(previousRow(index), previousRow(index + 1))
+        }
+      }
+      .head
+  }
 
+  private def prepareData(): List[List[Int]] =
+    loadFileAsLines().map(_.split(" ").map(_.toInt).toList)
 }
