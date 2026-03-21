631c77d7c540a54a19bfd91fa9bbef885038ba74
Detharon <detharon@gmail.com>
2026-03-21 21:23:44 +0100

    Add explanation comment for problem 39

--- src/main/scala/Euler039.scala
+++ src/main/scala/Euler039.scala
@@ -1,21 +1,19 @@
 object Euler039 extends EulerApp {
 
-  /**
-   * Problem 39: perimeter If 'p' is the perimeter of a right angle triangle with integral length sides,
-   * {a, b, c}, there are exactly three solutions for p = 120.
-   * {20, 48, 52}, {24, 45, 51}, {30, 40, 50}
-   *
-   * For which value of p <= 1000 is the number of solutions maximised?
-   *
-   * --------------------------------------------------
-   *
-   * We could just check all the possible values of a, b, c and check if they follow the Pythagorean theorem,
-   * a^2^ + b^2^ = c^2^, but we can reduce the number of possibilities to check since we also know that
-   * the sum of two length sides must be greater than the length of the third side, that is: a + b > c.
-   *
-   * For example, for a perimeter of 120, we know that one side has to be smaller than 60, because the sum of
-   * two sides has to be greater than the third side. The upper bound, then, is perimeter / 2 - 1.
-   */
+  /** Problem 39: perimeter If 'p' is the perimeter of a right angle triangle with integral length sides, {a, b, c},
+    * there are exactly three solutions for p = 120. {20, 48, 52}, {24, 45, 51}, {30, 40, 50}
+    *
+    * For which value of p <= 1000 is the number of solutions maximised?
+    *
+    * --------------------------------------------------
+    *
+    * We could just check all the possible values of a, b, c and check if they follow the Pythagorean theorem, a^2^ +
+    * b^2^ = c^2^, but we can reduce the number of possibilities to check since we also know that the sum of two length
+    * sides must be greater than the length of the third side, that is: a + b > c.
+    *
+    * For example, for a perimeter of 120, we know that one side has to be smaller than 60, because the sum of two sides
+    * has to be greater than the third side. The upper bound, then, is perimeter / 2 - 1.
+    */
   override def execute(): Int = (1 to 1000).maxBy { n =>
     findRightTriangles(n).size
   }
