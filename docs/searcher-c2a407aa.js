03be478a86e05a71f87834943a73270d5a3e2d73
Detharon <detharon@gmail.com>
2026-06-18 08:39:16 +0200

    Fix description for problem 28

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4d9fb6ba.js',
+                path_to_root + 'searchindex-18ee6855.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
