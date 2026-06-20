b42de5fbf054435fddea3f00850d1a28ca82f09e
Detharon <detharon@gmail.com>
2026-06-20 13:35:51 +0200

    Fix the error in problem 16 (^ is no longer implicitly turned into a power)

--- src/main/scala/Euler016.scala
+++ src/main/scala/Euler016.scala
@@ -1,3 +1,3 @@
 object Euler016 extends EulerApp {
-  override def execute(): Int = (BigInt(2) ^ 1000).toString().map(_.asDigit).sum
+  override def execute(): Int = BigInt(2).pow(1000).toString().map(_.asDigit).sum
 }
