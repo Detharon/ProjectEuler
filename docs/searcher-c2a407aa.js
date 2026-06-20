02b8f35599bdf7b9b2d421b09c75502afc49448f
Detharon <detharon@gmail.com>
2026-06-20 08:55:50 +0200

    Improve formatting of problem 29 explanation

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e93c349c.js',
+                path_to_root + 'searchindex-19faee4f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
