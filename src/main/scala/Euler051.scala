68865b9c3d1627767bea9b687356200d194de646
Detharon <detharon@gmail.com>
2026-07-11 08:40:47 +0200

    Format solution of problem 51

--- src/main/scala/Euler051.scala
+++ src/main/scala/Euler051.scala
@@ -2,11 +2,14 @@ import scala.annotation.tailrec
 
 object Euler051 extends EulerApp {
   override def execute() = {
-    LazyList.from(1).flatMap { digits =>
-      generatePrimeFamilies(digits).map { primeFamily =>
-        (primeFamily, primesCount(primeFamily))
+    LazyList
+      .from(1)
+      .flatMap { digits =>
+        generatePrimeFamilies(digits).map { primeFamily =>
+          (primeFamily, primesCount(primeFamily))
+        }
       }
-    }.find { case (primeFamily, primes) => primes == 8}
+      .find { case (primeFamily, primes) => primes == 8 }
   }
 
   private def generatePrimeFamilies(totalDigits: Int) = {
@@ -17,19 +20,28 @@ object Euler051 extends EulerApp {
     }
 
     @tailrec
-    def generate(totalDigits: Int, iteration: Int, wildcardIndex: Seq[Int], results: Seq[PrimeFamily]): Seq[PrimeFamily] = {
+    def generate(
+        totalDigits: Int,
+        iteration: Int,
+        wildcardIndex: Seq[Int],
+        results: Seq[PrimeFamily]
+    ): Seq[PrimeFamily] = {
       if (iteration == totalDigits) results
       else {
         val newDigits: Seq[DigitOrWildcard] =
           if (wildcardIndex.contains(iteration)) Seq(Wildcard)
-          else (if (iteration == totalDigits - 1) 1 to 9 else 0 to 9).map(Character.forDigit(_, 10))
+          else
+            (if (iteration == totalDigits - 1) 1 to 9 else 0 to 9)
+              .map(Character.forDigit(_, 10))
 
         val newResults: Seq[PrimeFamily] =
-          if (results.isEmpty) newDigits.map(newDigit => PrimeFamily(Seq(newDigit)))
-          else for {
-            newDigit <- newDigits
-            result <- results
-          } yield PrimeFamily(newDigit +: result.digits)
+          if (results.isEmpty)
+            newDigits.map(newDigit => PrimeFamily(Seq(newDigit)))
+          else
+            for {
+              newDigit <- newDigits
+              result <- results
+            } yield PrimeFamily(newDigit +: result.digits)
 
         generate(totalDigits, iteration + 1, wildcardIndex, newResults)
       }
@@ -51,18 +63,21 @@ object Euler051 extends EulerApp {
   private case object Wildcard
   private case class PrimeFamily(digits: Seq[DigitOrWildcard]) {
     override def toString: String = digits.map {
-      case c: Char => c
+      case c: Char  => c
       case Wildcard => '*'
     }.mkString
 
     def replaceWildcard(digit: Char): Int =
-      digits.map {
-        case c: Char => c
-        case Wildcard => digit
-      }.mkString.toInt
+      digits
+        .map {
+          case c: Char  => c
+          case Wildcard => digit
+        }
+        .mkString
+        .toInt
   }
 
   private def isPrime(n: Long): Boolean = n match
     case n if n < 2 => false
-    case _ => (2 to Math.sqrt(n.toDouble).toInt).forall(n % _ != 0)
+    case _          => (2 to Math.sqrt(n.toDouble).toInt).forall(n % _ != 0)
 }
